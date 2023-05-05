04bf4a0b0f3b39fa283711187d01727a25ab1bfc
Anastasiia Derdiai <115905772+nastiona9339@users.noreply.github.com>
2023-05-05 14:39:24 -0500

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const callToGallary = document.querySelector(".gallery")
+
+function creatItem() {
+    let  elements = ""
+        
+    galleryItems.forEach(item => 
+        elements += `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+  </a>
+</li>`)
+    
+    callToGallary.innerHTML = elements
+}
+
+creatItem()
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay : 250 });
+lightbox.on('show.simplelightbox', function () {
+});
+
 console.log(galleryItems);
